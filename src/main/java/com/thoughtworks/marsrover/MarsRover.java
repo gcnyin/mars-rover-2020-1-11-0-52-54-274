846e2fb05ac47fe59791d4453c78552225c6c716
gcnyin <guchounongyin@gmail.com>
2020-01-12 21:19:17 +0800

    make should_accept_initialization_message_and_send_own_position_and_direct pass

--- src/main/java/com/thoughtworks/marsrover/MarsRover.java
+++ src/main/java/com/thoughtworks/marsrover/MarsRover.java
@@ -6,8 +6,14 @@ import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 public class MarsRover extends AbstractBehavior<MarsRover.Command> {
+    private double x;
+    private double y;
+    private Direct direct;
+
     public interface Command {
     }
 
@@ -21,17 +27,46 @@ public class MarsRover extends AbstractBehavior<MarsRover.Command> {
 
     @Override
     public Receive<Command> createReceive() {
-        return newReceiveBuilder().build();
+        return newReceiveBuilder()
+                .onMessage(Initialization.class, this::onInitialization)
+                .build();
     }
 
-    public static class ReceivePositionAndDirect {
-        public ReceivePositionAndDirect(double x, double y, Direct direct) {
+    public Behavior<Command> onInitialization(Initialization initialization) {
+        x = initialization.x;
+        y = initialization.y;
+        direct = initialization.direct;
+        initialization.replyTo.tell(new ReceivePositionAndDirect(x, y, direct));
+        return this;
+    }
 
+    @ToString
+    @EqualsAndHashCode
+    public static class ReceivePositionAndDirect {
+        public final double x;
+        public final double y;
+        public final Direct direct;
+
+        public ReceivePositionAndDirect(double x, double y, Direct direct) {
+            this.x = x;
+            this.y = y;
+            this.direct = direct;
         }
     }
 
+    @ToString
+    @EqualsAndHashCode
     public static class Initialization implements Command {
+        public final double x;
+        public final double y;
+        public final Direct direct;
+        public final ActorRef<ReceivePositionAndDirect> replyTo;
+
         public Initialization(double x, double y, Direct direct, ActorRef<ReceivePositionAndDirect> replyTo) {
+            this.x = x;
+            this.y = y;
+            this.direct = direct;
+            this.replyTo = replyTo;
         }
     }
 }
