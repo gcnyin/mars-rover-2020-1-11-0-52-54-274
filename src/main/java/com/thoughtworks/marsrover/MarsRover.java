64fedbd7c14adf1cb21374c28a056a274f1992bc
gcnyin <guchounongyin@gmail.com>
2020-01-14 13:16:52 +0800

    [second round] make should_return_rover_status pass

--- src/main/java/com/thoughtworks/marsrover/MarsRover.java
+++ src/main/java/com/thoughtworks/marsrover/MarsRover.java
@@ -2,12 +2,34 @@ package com.thoughtworks.marsrover;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.List;
 
-public class MarsRover {
+public class MarsRover extends AbstractBehavior<MarsRover.Command> {
     public static Behavior<Command> create() {
-        return null;
+        return Behaviors.setup(MarsRover::new);
+    }
+
+    public MarsRover(ActorContext<Command> context) {
+        super(context);
+    }
+
+    @Override
+    public Receive<Command> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(BatchMessage.class, this::onBatchMessage)
+                .build();
+    }
+
+    private Behavior<Command> onBatchMessage(BatchMessage a) {
+        a.replyTo.tell(new Status(10.0, 20.0, Direct.N));
+        return this;
     }
 
     public interface Command {
@@ -19,12 +41,26 @@ public class MarsRover {
     }
 
     public static class BatchMessage implements Command {
+        public final ActorRef<Status> replyTo;
+        public final List<Command> messages;
+
         public BatchMessage(ActorRef<Status> replyTo, List<Command> messages) {
+            this.replyTo = replyTo;
+            this.messages = messages;
         }
     }
 
+    @ToString
+    @EqualsAndHashCode
     public static class Status {
-        public Status(double x, double y, Direct direct) {
+        public final Double x;
+        public final Double y;
+        public final Direct direct;
+
+        public Status(Double x, Double y, Direct direct) {
+            this.x = x;
+            this.y = y;
+            this.direct = direct;
         }
     }
 }
